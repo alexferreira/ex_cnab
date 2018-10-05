007d44da687ca8863fcff3698cd5f6c2554353e0
JPSO18 <joao.paulojp18@hotmail.com>
2018-10-05 18:30:31 -0300

    Added fields in template to Reader, so it can parse segment b from payment_on_checking properly

--- test/cnab/decoder_test.exs
+++ test/cnab/decoder_test.exs
@@ -1,9 +1,15 @@
 defmodule ExCnab.Test.CNAB.DecoderTest do
     use ExCnab.Test.Support
 
-    import ExCnab.Test.Support.Factory, only: [cnab_document: 0]
+    import ExCnab.Test.Support.Factory, only: [cnab: 1]
 
-    test "Do: Decode CNAB" do
-        assert _cnab = CNAB.Decoder.decode(cnab_document())
-    end
+    @payment_several :cnab_payment_several
+    @ted_payment :cnab_ted_payment
+
+    [@payment_several, @ted_payment]
+    |> Enum.each(fn payment_type ->
+        test "Do: Decode CNAB #{payment_type}" do
+            assert _cnab = CNAB.Decoder.decode(cnab(unquote(payment_type)))
+        end
+    end)
 end
--- test/cnab/reader_test.exs
+++ test/cnab/reader_test.exs
@@ -2,10 +2,13 @@ defmodule ExCnab.Test.CNAB.ReaderTest do
 
     use ExCnab.Test.Support
 
-    test "Do: read a cnab file" do
-        cnab_path = :code.priv_dir(:ex_cnab) |> Path.join("cnabs/cnab_payment_several")
-        assert {:ok, _data} = CNAB.Reader.read_cnab(cnab_path)
-    end
+    [:ted_payment, :payment_several]
+    |> Enum.each(fn payment_type ->
+        test "Do: read a cnab #{payment_type} file" do
+            cnab_path = :code.priv_dir(:ex_cnab) |> Path.join("cnabs/cnab_#{unquote(payment_type)}")
+            assert {:ok, _data} = CNAB.Reader.read_cnab(cnab_path)
+        end
+    end)
 
     test "Do: read a statement cnab file" do
         statement_cnab_path = :code.priv_dir(:ex_cnab) |> Path.join("cnabs/statement_cnab")
